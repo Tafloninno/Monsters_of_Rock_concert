3deb2b3766af3d05531ee4581f5f45f3d28b478b
Mugisha Innocent <111200646+Tafloninno@users.noreply.github.com>
2023-02-10 15:51:51 +0300

    made changes

--- index.js
+++ index.js
@@ -16,36 +16,42 @@ const ArtistsItems = [
   {
     ArtistName: 'Jon Bon Jovi',
     image: 'images/artist_1.webp',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
   {
     ArtistName: 'Chuck Berry',
     image: 'images/artist_3.jpg',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
   {
     ArtistName: 'Richard Anthony',
     image: 'images/rock_artist3.jpeg',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
   {
     ArtistName: 'The Brian Setzer Orchestra',
     image: 'images/artist_2.avif',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
   {
     ArtistName: 'The Big Bopper',
     image: 'images/rock_artist4.webp',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
   {
     ArtistName: 'Johnny Burnette',
     image: 'images/artist_yellow.jpeg',
+    alt: 'image',
     Description: "arttist,songwriter ,band did in fact invent rock'n'roll",
     Paragraph: "Charles' proto-soul vocals and Brown's everything-is-a-drum were innovations as profound as Berry's ",
   },
@@ -54,10 +60,10 @@ const ArtistsItems = [
 if (ArtistsCards) {
   for (let i = 0; i < ArtistsItems.length; i += 1) {
     ArtistsCards.innerHTML += `
-    <div class="hidden_artists">
-    <div class="artist_details">
+    <article class="hidden_artists">
+    <article class="artist_details">
         <div class="band_image">
-            <img src="${ArtistsItems[i].image}" alt="" class="normal_image_desk">
+            <img src="${ArtistsItems[i].image}" alt="${ArtistsItems[i].alt}" class="normal_image_desk">
         </div>
         <div class="artist_description">
             <h2>${ArtistsItems[i].ArtistName}</h2>
@@ -65,8 +71,8 @@ if (ArtistsCards) {
             <hr>
             <p>${ArtistsItems[i].Paragraph}</p>
         </div>
-     </div>
-  </div>
+     </article>
+  </article>
     `;
   }
 }
